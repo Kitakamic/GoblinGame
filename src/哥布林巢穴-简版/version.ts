4d429264ca7d448924db7ec613e974c7b3b617bc
Kitakamic <236963184+Kitakamic@users.noreply.github.com>
2025-11-22 16:05:15 +0800

    fix: 更新 FRONTEND_PUSH_INDEX_HTML 为 true，以启用 index.html 的推送功能

--- src/哥布林巢穴-简版/version.ts
+++ src/哥布林巢穴-简版/version.ts
@@ -7,4 +7,4 @@ export const FRONTEND_VERSION = '1.5.4.2';
 export const FRONTEND_UPDATE_DATE = '2025-11-15';
 export const FRONTEND_DESCRIPTION = '最新版本';
 export const FRONTEND_VERSION_TYPE: 'stable' | 'beta' = 'stable';
-export const FRONTEND_PUSH_INDEX_HTML: boolean = false;
+export const FRONTEND_PUSH_INDEX_HTML: boolean = true;
